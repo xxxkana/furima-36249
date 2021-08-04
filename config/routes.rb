ab285f9d5a46e86375dc4ec1500674aa36d42fd4
xxxkana <kana_m.0421@icloud.com>
2021-08-04 12:46:13 +0900

    商品一覧表示機能修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
-  resources :items, only: [:index, :new, :create, :show, :edit, :destroy] 
+  resources :items, only: [:index, :new, :create] 
   
 end
