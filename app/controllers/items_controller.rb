0ef4f00749a22f6ca4405bfae2a2d2553abe7e4f
xxxkana <kana_m.0421@icloud.com>
2021-08-05 15:05:22 +0900

    商品詳細編集機能実装2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index, only: [:edit, :update, :destroy]
 
+  
   def index
     @items = Item.order("created_at DESC")
 
@@ -25,12 +27,18 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    @item = Item.edit(item_params)
-    if @item.save
-      redirect_to root_path
+    @item= Item.find(params[:id])
+  
+  end  
+
+  def update
+    @item= Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item)
     else
       render :edit
     end
+
   end  
 
   def destroy
@@ -42,4 +50,10 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :name, :comments, :price, :category_id, :condition_id, :delivery_fee_id, :prefecture_id, :shipment_day_id).merge(user_id: current_user.id)
   end
+
+  def move_to_index
+    @item= Item.find(params[:id])
+    redirect_to root_path unless current_user == @item.user
+  end
+  
 end
