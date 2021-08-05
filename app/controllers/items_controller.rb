93543388a40796c35e7c19eae0a2fba9e8a0d427
xxxkana <kana_m.0421@icloud.com>
2021-08-05 16:40:28 +0900

    商品詳細編集機能修正4

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_index, only: [:edit, :update, :destroy]
+  before_action :move_to_index, only: [:edit, :update]
   before_action :set_item, :move_to_index, only: [:show, :edit, :update]
   
   def index
