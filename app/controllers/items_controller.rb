a2c71c74ee1d48b765ab11ec5cca57fb49d91ac2
xxxkana <kana_m.0421@icloud.com>
2021-08-06 19:02:25 +0900

    商品削除機能修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
-  before_action :move_to_index, only: [:edit, :update]
+  before_action :move_to_index, only: [:edit, :update, :destroy]
   
   def index
     @items = Item.order("created_at DESC")
@@ -41,7 +41,7 @@ class ItemsController < ApplicationController
       if @item.destroy
         redirect_to root_path
       else
-        render :edit
+        render :show
       end
   end  
 
