68a5e663fc4a472c760fae117beb97bdb785553a
xxxkana <kana_m.0421@icloud.com>
2021-08-08 22:31:27 +0900

    商品購入機能実装2

--- app/controllers/user_item_controller.rb
+++ app/controllers/user_item_controller.rb
@@ -2,14 +2,19 @@ class UserItemController < ApplicationController
   before_action :authenticate_user!, except: :index
 
   def index
-    @user_item =UserItem.new
+    @item = Item.find(params[:item_id])
+    @user_item =AddressUserItem.new
+
   end  
 
   
   def create
-    @user_item = UserItem.new(user_item_params)
+    @item = Item.find(params[:item_id])
+    @user_item = AddressUserItem.new(user_item_params)
     if @user_item.valid?
+      pay_item
       @user_item.save
+     
       redirect_to root_path
     else
       render :index
@@ -20,8 +25,18 @@ class UserItemController < ApplicationController
   private
 
   def user_item_params
-    params.require(:user_item).permit(:postal_code, :prefecture, :city, :house_number, :building_name).merge(user_id: current_user.id)
+    params.require(:address_user_item).permit(:postal_code, :prefecture_id, :city, :house_number, :building_number, :phone_number).merge(user_id: current_user.id, token: params[:token])
   end  
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp.api_key = "sk_test_50e0bd5213eacb07f04a8494"
+    Payjp::Charge.create(
+      amount: order_params[:price],
+      card: order_params[:token],
+      currency: 'jpy'
+    )
+  end
   
 
 end
