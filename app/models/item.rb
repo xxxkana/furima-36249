bf479fd3100f25994270b49c51d51e282f41a36d
xxxkana <kana_m.0421@icloud.com>
2021-08-13 12:49:42 +0900

    エラーコードの日本語化実装２

--- app/models/item.rb
+++ app/models/item.rb
@@ -17,7 +17,7 @@ class Item < ApplicationRecord
     validates :comments, length: {maximum: 1000}
  end
 
-  validates :category_id, :condition_id, :delivery_fee_id, :prefecture_id, :shipment_day_id, numericality: { other_than: 1, message: "can't be blank"} 
+  validates :category_id, :condition_id, :delivery_fee_id, :prefecture_id, :shipment_day_id, numericality: { other_than: 1, message: "を選択してください"} 
 
   validates :price, numericality: {greater_than: 299, less_than: 10000000}, format: {with: /\A[0-9]+\z/}
 end
